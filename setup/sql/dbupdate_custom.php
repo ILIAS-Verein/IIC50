e094c8be58878972a2d287bba23c683b56ecdf86
Alex Killing <alex.killing@gmx.de>
2015-04-27 22:39:36 +0200

    merged leifos/5_0_carousel_a14k435

---
+++ setup/sql/dbupdate_custom.php
@@ -0,0 +1,24 @@
+<#1>
+<?php
+include_once("./Services/Migration/DBUpdate_3136/classes/class.ilDBUpdate3136.php");
+ilDBUpdate3136::addStyleClass("CarouselCntr", "ca_cntr", "div",
+	array());
+?>
+<#2>
+<?php
+include_once("./Services/Migration/DBUpdate_3136/classes/class.ilDBUpdate3136.php");
+ilDBUpdate3136::addStyleClass("CarouselICntr", "ca_icntr", "div",
+	array());
+?>
+<#3>
+<?php
+include_once("./Services/Migration/DBUpdate_3136/classes/class.ilDBUpdate3136.php");
+ilDBUpdate3136::addStyleClass("CarouselIHead", "ca_ihead", "div",
+	array());
+?>
+<#4>
+<?php
+include_once("./Services/Migration/DBUpdate_3136/classes/class.ilDBUpdate3136.php");
+ilDBUpdate3136::addStyleClass("CarouselICont", "ca_icont", "div",
+	array());
+?>

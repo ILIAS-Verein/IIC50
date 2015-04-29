9b6b49a29d351f21fa4e5f218907a54df2f2d658
Alex Killing <alex.killing@gmx.de>
2015-04-29 11:55:07 +0200

    autlink adv metadata values in wiki side block

---
+++ setup/sql/dbupdate_custom.php
@@ -0,0 +1,25 @@
+<#1>
+<?php
+//
+?>
+<#2>
+<?php
+//
+?>
+<#3>
+<?php
+//
+?>
+<#4>
+<?php
+//
+?>
+<#5>
+<?php
+$ilDB->addTableColumn("il_wiki_data", "link_md_values",array (
+	"type" => "integer",
+	"length" => 1,
+	"notnull" => false,
+	"default" => 0,
+));
+?>
